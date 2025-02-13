d4dbf5e0e6123bddb71580c60fb9de94313097a7
Milton Jonathan <mjonathan.cartesi@gmail.com>
2025-02-13 12:52:01 -0300

    feat: add EigenLayerConsensusServiceManager impl

--- script/EigenLayerConsensus.s.sol
+++ script/EigenLayerConsensus.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {EigenLayerConsensus} from "../src/EigenLayerConsensus.sol";
+import {IEigenLayerConsensusServiceManager} from "../src/IEigenLayerConsensusServiceManager.sol";
 
 contract EigenLayerConsensusScript is Script {
     EigenLayerConsensus public consensus;
@@ -12,7 +13,7 @@ contract EigenLayerConsensusScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        consensus = new EigenLayerConsensus(10, 10);
+        consensus = new EigenLayerConsensus(IEigenLayerConsensusServiceManager(address(0)), 10, 10);
 
         vm.stopBroadcast();
     }
--- test/EigenLayerConsensus.t.sol
+++ test/EigenLayerConsensus.t.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {EigenLayerConsensus} from "../src/EigenLayerConsensus.sol";
+import {IEigenLayerConsensusServiceManager} from "../src/IEigenLayerConsensusServiceManager.sol";
 
 contract EigenLayerConsensusTest is Test {
     EigenLayerConsensus public consensus;
 
     function setUp() public {
-        consensus = new EigenLayerConsensus(10, 10);
+        consensus = new EigenLayerConsensus(IEigenLayerConsensusServiceManager(address(0)), 10, 10);
     }
 
     function testFuzz_NotInFavor(
